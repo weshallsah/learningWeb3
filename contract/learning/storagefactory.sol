61ccc280d9aba1ffdda5f5b9ca40f5b4d87a859f
weshallsah <vishalk74064@gmail.com>
2024-08-27 19:53:28 +0530

    init

---
+++ contract/learning/storagefactory.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {SimpleStorage} from "./simpelstorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public simplestorage;
+
+    function createSimpelStorage() public {
+        simplestorage.push(new SimpleStorage());
+    }
+    function sfstore(uint256 _ssidx,uint256 _newssnumber) public {
+        // ABI = application binary interface
+        SimpleStorage mysimplestorage = simplestorage[_ssidx];
+        mysimplestorage.store(_newssnumber);
+    }
+    function sfget(uint256 _ssidx) public view returns(uint256){
+        SimpleStorage mysimplestorage = simplestorage[_ssidx];
+        return mysimplestorage.retrieve();
+    } 
+}
